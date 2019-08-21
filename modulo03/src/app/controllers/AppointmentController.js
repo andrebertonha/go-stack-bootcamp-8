f84688570e174b0e26c16e6872dd82da6ebf755c
André Martins <andrebertonhadev@gmail.com>
2019-08-21 12:58:01 -0300

    feature: notification when an appointment is canceled

--- modulo03/src/app/controllers/AppointmentController.js
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -7,6 +7,8 @@ import User from '../models/User';
 import Notification from '../schemas/Notification';
 import { Op } from 'sequelize';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
 
   async index(req, res) {
@@ -114,7 +116,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          atributes: ['name', 'email']
+        },
+      ],
+    });
 
     if(appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -128,9 +138,14 @@ class AppointmentController {
     }
 
     appointment.canceled_at = new Date();
-    console.log(appointment.canceled_at);
 
-    await appointment.save(appointment);
+    await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento'
+    });
 
     return res.json(appointment);
   }
---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "581fa7ee175cd3",
+    pass: "bf82554f8f62a3"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+};
+
+/**
+ * after deply have to use
+ * AMAZON SES
+ * Mail gun
+ * Spark post
+ */
