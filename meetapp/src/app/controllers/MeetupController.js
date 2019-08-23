04b25ded413c50c78a06e71e697501ceccc2152a
André Martins <andrebertonhadev@gmail.com>
2019-08-23 18:41:17 -0300

    chore: meetup update

--- meetapp/src/app/controllers/MeetupController.js
+++ meetapp/src/app/controllers/MeetupController.js
@@ -16,7 +16,8 @@ class MeetupController {
         [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
       };
     }
-    const meetups = Meetup.findAll({
+
+    const meetups = await Meetup.findAll({
       where,
       include: [User],
       limit: 10,
@@ -50,7 +51,67 @@ class MeetupController {
       user_id,
     });
 
+    const { title, description, location, date } = meetup;
+    return res.json({
+      meetup: {
+        title,
+        description,
+        location,
+        date,
+      },
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      file_id: Yup.number(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const user_id = req.userId;
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== user_id) {
+      return res.status(401).json({ error: 'Not authorized.' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({ error: 'Meetup date invalid' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't update past meetups." });
+    }
+
+    await meetup.update(req.body);
+
     return res.json(meetup);
+  }
+
+  async delete(req, res) {
+    const user_id = req.userId;
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== user_id) {
+      return res.status(401).json({ error: 'Not authorized.' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't delete past meetups." });
+    }
+
+    await meetup.destroy();
+
+    return res.send();
   }
 }
 
