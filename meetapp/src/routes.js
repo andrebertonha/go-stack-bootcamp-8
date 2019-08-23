fb3812caaeaaec6b99124a85ab5f73d27c052aee
André Martins <andrebertonhadev@gmail.com>
2019-08-23 13:01:13 -0300

    feat: file upload

--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +18,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
 
 export default routes;
