a5f0046fbbb1b50297c59b76ff00c5c67cb873ac
André Martins <andrebertonhadev@gmail.com>
2019-08-24 10:17:40 -0300

    feat: meetapp user subscription

--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -8,6 +8,8 @@ import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 import MeetupController from './app/controllers/MeetupController';
+import SubscriptionController from './app/controllers/SubscriptionController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,5 +27,11 @@ routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizing', OrganizingController.index);
+
+routes.get('/subscriptions', SubscriptionController.index);
+
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
 
 export default routes;
