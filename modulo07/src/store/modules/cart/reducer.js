ca74377c1beb27c40f2b166d51bf28aed9d91740
André Martins <andrebertonhadev@gmail.com>
2019-09-02 21:45:14 -0300

    refatorando as actions

--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case '@cart/ADD':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.product.id);
 
@@ -16,11 +16,11 @@ export default function cart(state = [], action) {
         }
       });
 
-    case 'REMOVE_FROM_CART':
+    case '@cart/REMOVE':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 
-        if (productIndex > 0) {
+        if (productIndex >= 0) {
           draft.splice(productIndex, 1);
         }
       });
