da4d90c9b6d72938ce80c4e31a3f90edc407c6ba
André Martins <andrebertonhadev@gmail.com>
2019-09-04 10:13:05 -0300

    React Hooks: useState, useEffect, useMemo

---
+++ modulo08/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+function App() {
+  const [tech, setTech] = useState([]);
+
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  // did mount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // did update
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;
