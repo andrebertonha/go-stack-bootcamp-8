0a7ee1e717b335634fc1bd2c89724919de50592c
André Martins <andrebertonhadev@gmail.com>
2019-09-10 10:13:39 -0300

    show toasts

--- modulo09/src/store/modules/auth/sagas.js
+++ modulo09/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 
@@ -17,12 +18,13 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário nao é prestador');
+      toast.error('Usuário nao é prestador');
     }
 
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
