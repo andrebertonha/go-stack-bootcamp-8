85bebac06cbee1edcf778b7ed55539da9fafb44f
André Martins <andrebertonhadev@gmail.com>
2019-09-10 10:46:33 -0300

    user sign up

--- modulo09/src/store/modules/auth/sagas.js
+++ modulo09/src/store/modules/auth/sagas.js
@@ -2,8 +2,8 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import history from '~/services/history';
-
 import api from '~/services/api';
+
 import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
@@ -29,4 +29,24 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
