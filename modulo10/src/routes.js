d6d097d08908169673da2ddd41498119b85680ea
André Martins <andrebertonhadev@gmail.com>
2019-09-19 18:30:23 -0300

    schedule routes

--- modulo10/src/routes.js
+++ modulo10/src/routes.js
@@ -1,31 +1,76 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from '~/pages/New/SelectProvider';
+import SelectDateTime from '~/pages/New/SelectDateTime';
+import Confirm from '~/pages/New/Confirm';
 
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 
-export default (isSigned = false) => createAppContainer(
-  createSwitchNavigator({
-    Sign: createSwitchNavigator({
-      SignIn,
-      SignUp,
-    }),
-    App: createBottomTabNavigator({
-      Dashboard,
-      Profile,
-    }, {
-      tabBarOptions: {
-        keyboardHidesTabBar: true,
-        activeTintColor: '#FFF',
-        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
-        style: {
-          backgroundColor: '#8d41aB',
-        },
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, .09)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41aB',
+              },
+            },
+          }
+        ),
       },
-    }),
-  }, {
-    initialRouteName: isSigned ? 'App' : 'Sign',
-  })
-);
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
