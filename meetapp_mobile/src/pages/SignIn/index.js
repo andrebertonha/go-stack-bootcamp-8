8fb698b3a4de6612a75d103dc84fbb7eb53ba0e2
André Martins <andrebertonhadev@gmail.com>
2019-09-20 22:57:57 -0300

    Sign in page

--- meetapp_mobile/src/pages/SignIn/index.js
+++ meetapp_mobile/src/pages/SignIn/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+
+import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>Signin</Text>
+      <Container>
+        <Image source={logo} alt="Meet App" />
+        <Form>
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>
+            Entrar
+          </SubmitButton>
+        </Form>
+      </Container>
     </Background>
   );
 }
