47562cf8da34883f93d5ee78edfbf6f69b460b7d
André Martins <andrebertonhadev@gmail.com>
2019-09-27 23:21:36 -0300

    create bottom tab navigator and import background

--- meetapp_mobile/src/pages/Dashboard/index.js
+++ meetapp_mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Backgroud from '~/components/Background';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  return <Backgroud />;
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Inscrições',
+  tabBarIcon: ({tintColor}) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
