bf2aedf16f7e11319c6d864e9a681518e43ba2c2
André Martins <andrebertonhadev@gmail.com>
2019-11-08 22:59:00 -0300

    rate limit with express-rate-limit and rate-limit-redis

--- modulo16/src/app.js
+++ modulo16/src/app.js
@@ -4,6 +4,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 
 import 'express-async-errors';
@@ -30,10 +33,26 @@ class App {
     this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if(process.env.NODE_ENV !== 'development') {
+      // x tentativas de acesso de determinado ip
+      this.server.use(new RateLimit({
+        store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            })
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
