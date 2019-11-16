b6f505184d7c148090dc927e0300d70491cc26db
André Martins <andrebertonhadev@gmail.com>
2019-11-16 13:59:43 -0300

    relacionamentos

--- bonus/adonisjs-gotstack/gonode/app/Models/Project.js
+++ bonus/adonisjs-gotstack/gonode/app/Models/Project.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class Project extends Model {
+  // relacionamento project -> usuario
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  // relacionamento project -> tarefa
+  tasks () {
+    return this.hasMany('App/Models/Task')
+  }
 }
 
 module.exports = Project
